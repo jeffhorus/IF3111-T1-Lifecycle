5ab18eea0ceaea0d8f88ffaa0cbe8b62359ea932
jeffhorus <jeffhorus19@gmail.com>
2015-02-07 22:09:52 +0700

    Selesai tutorial 1 dan 2a

---
+++ AndroidLifecycleActivity/app/src/main/java/jeffhorus/androidlifecycleactivity/SecondActivity.java
@@ -0,0 +1,20 @@
+package jeffhorus.androidlifecycleactivity;
+
+/**
+ * Created by Jelink on 2/7/2015.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SecondActivity extends TracerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+
+}
