458337546e32401f432282ba3084b27b1b68dcfb
jeffhorus <jeffhorus19@gmail.com>
2015-02-08 13:53:25 +0700

    Final final commit : update spinner

--- AndroidLifecycleActivity/app/src/main/java/jeffhorus/androidlifecycleactivity/MainActivity.java
+++ AndroidLifecycleActivity/app/src/main/java/jeffhorus/androidlifecycleactivity/MainActivity.java
@@ -16,7 +16,12 @@ public class MainActivity extends TracerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // configure the spinner in code
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String[] values = getResources().getStringArray(R.array.operating_systems);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        spinner.setAdapter(adapter);
     }
 
     public void onClick(View view) {
